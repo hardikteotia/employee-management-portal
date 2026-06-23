c7c89bbe4efba7f732ae87201c986abe2c32afd7
Hardik Teotia <hardik10teotia@gmail.com>
2026-06-23 18:30:48 +0530

    added employee project and user entities

---
+++ employee-management-portal/src/main/java/com/laneway/empportal/entity/User.java
@@ -0,0 +1,32 @@
+package com.laneway.empportal.entity;
+
+import com.laneway.empportal.enums.UserRole;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    private UserRole role;
+
+    @OneToOne
+    @JoinColumn(name = "employee_id")
+    private Employee employee;
+}
---
+++ employee-management-portal/src/main/java/com/laneway/empportal/repository/EmployeeProjectRepository.java
@@ -0,0 +1,13 @@
+package com.laneway.empportal.repository;
+
+import com.laneway.empportal.entity.EmployeeProject;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface EmployeeProjectRepository
+        extends JpaRepository<EmployeeProject, Long> {
+
+    List<EmployeeProject> findByEmployeeId(Long employeeId);
+
+}
