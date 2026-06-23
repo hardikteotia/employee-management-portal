ca3d08877b2e8a09fed715972bb178a09b1d0a2c
Hardik Teotia <hardik10teotia@gmail.com>
2026-06-23 18:20:28 +0530

    Add employee entity and employeeRepository

---
+++ employee-management-portal/src/main/java/com/laneway/empportal/entity/Employee.java
@@ -0,0 +1,55 @@
+package com.laneway.empportal.entity;
+
+import com.laneway.empportal.enums.EmploymentStatus;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Entity
+@Table(name = "employees")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    private String designation;
+
+    private LocalDate dateOfJoining;
+
+    private String timezone;
+
+    @Enumerated(EnumType.STRING)
+    private EmploymentStatus employmentStatus;
+
+    @Builder.Default
+    private Boolean deleted = false;
+
+    @ManyToOne
+    @JoinColumn(name = "department_id")
+    private Department department;
+
+    @ManyToOne
+    @JoinColumn(name = "location_id")
+    private Location location;
+
+    @ManyToOne
+    @JoinColumn(name = "manager_id")
+    private Employee manager;
+
+    @OneToMany(mappedBy = "manager")
+    private List<Employee> directReports;
+}
